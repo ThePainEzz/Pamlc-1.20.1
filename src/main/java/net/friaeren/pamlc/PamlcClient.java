befde479442a5a40989f59750deff295318308f4
ThePainEzz <montecchia.fra@gmail.com>
2025-08-09 06:01:04 +0200

    First Commit

---
+++ src/main/java/net/friaeren/pamlc/PamlcClient.java
@@ -0,0 +1,10 @@
+package net.friaeren.pamlc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PamlcClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
